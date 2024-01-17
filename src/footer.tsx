f7d7117c63f61fe17c0fa7d633d096ea40a596b5
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-17 14:39:33 +0100

    updated

--- src/footer.tsx
+++ src/footer.tsx
@@ -1,31 +1,34 @@
 import React from "react";
 
-function Myfooter(){
-    return(
+function Myfooter() {
+    return (
         <>
-       <div className="holux-des">
-        <h5>BSA HOMES</h5>
-        <p>Our team of seasoned professionals is committed to providing you with exceptional service, in-depth market knowledge, and personalized solutions tailored to your unique needs.</p>
-       </div>
-       <div>
-        <h5>Company Info</h5>
-        <p>Home</p>
-        <p>About</p>
-        <p>Project</p>
-        <p>Contact</p>
-       </div>
-       <div>
-        <h5>Resources</h5>
-        <p>Terms</p>
-        <p>Conditions</p>
-        <p>Policy</p>
-       </div>
-       <div>
-        <h5>Contact</h5>
-        <p>bsahomes@gmail.com</p>
-        <p>+2347045756160</p>
-        
-       </div>
+            <div className="footer-wrapper">
+                <div className="bsa-mission">
+                    <h5>BSA HOMES</h5>
+                    <p>Our team of seasoned professionals is committed to providing you with exceptional service, in-depth market knowledge, and personalized solutions tailored to your unique needs.</p>
+                </div>
+                <div>
+                    <h5>Company Info</h5>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Project</p>
+                    <p>Contact</p>
+                </div>
+                <div>
+                    <h5>Resources</h5>
+                    <p>Terms</p>
+                    <p>Conditions</p>
+                    <p>Policy</p>
+                </div>
+                <div>
+                    <h5>Contact</h5>
+                    <p>bsahomes@gmail.com</p>
+                    <p>+2347045756160</p>
+
+                </div>
+                <p>Copyright © 2023 Mars Tchnologies. All rights reserved.</p>
+            </div>
         </>
     )
 }
