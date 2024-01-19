7c18f10f7f56acc29f3ea84fba3f904bcf6be8e6
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-19 15:27:33 +0100

    updated

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import "./footer.css"
 
 function Myfooter() {
     return (
         <>
             <div className="footer-wrapper">
-                <div className="bsa-mission">
+                <div className="bsa-mission ">
                     <h5>BSA HOMES</h5>
                     <p>Our team of seasoned professionals is committed to providing you with exceptional service, in-depth market knowledge, and personalized solutions tailored to your unique needs.</p>
                 </div>
-                <div>
+                <div className="section">
                     <h5>Company Info</h5>
                     <p>Home</p>
                     <p>About</p>
                     <p>Project</p>
                     <p>Contact</p>
                 </div>
-                <div>
+                <div className="section">
                     <h5>Resources</h5>
                     <p>Terms</p>
                     <p>Conditions</p>
                     <p>Policy</p>
                 </div>
-                <div>
+                <div className="section">
                     <h5>Contact</h5>
                     <p>bsahomes@gmail.com</p>
                     <p>+2347045756160</p>
