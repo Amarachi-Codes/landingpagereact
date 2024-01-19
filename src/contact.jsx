b6f13a415bcdeba869e291c1c4d95324c9312aca
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-19 15:15:00 +0100

    updated contactsection

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import "./contact.css";
 import contact from "./images/contact.jpg";
+import "./hero.css";
 
 function Contact(){
     return(
         <>
         <div className="contact-wrapper">
-            <div className="img-wrapper">
+            <div className="contact-img">
         <img src={contact} alt="contact" />
         </div>
         <div className="contact-form">
-            <h2>Contact Us</h2>
+            <h1>Contact Us</h1>
             <p>Discover the difference, where expertise meets excellence in real estate. Let's embark on this exciting journey together.</p>
-            <form action="">
+            <form action="" className="form">
             <input type="text" name="fullname" placeholder="FullName" /><br/>
             <input type="email" name="email" placeholder="Email Address" /><br/>
             <input type="number" name="phone" placeholder="Phone Number"/><br/>
             <textarea name="description" placeholder="Description" cols={30} rows={4}></textarea><br/>
-            <button>Send Message</button>
+            <button className="hero-btn">Send Message</button>
             </form>
         </div>
         </div>
